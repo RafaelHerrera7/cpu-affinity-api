7bf4bff24bdc946ce6bfecaa271c55d08fd37787
Rafael Herrera <131188388+RafaelHerrera7@users.noreply.github.com>
2026-05-25 13:14:16 -0400

    Use git describe --tags --always for version string

--- dev.bat
+++ dev.bat
@@ -1,4 +1,4 @@
 @echo off
 taskkill /F /IM goapi.exe 2>nul
-for /f "tokens=*" %%i in ('git rev-parse --short HEAD') do set GIT_HASH=%%i
-go build -ldflags "-H windowsgui -X goapi/api.Version=%GIT_HASH%" -o goapi.exe . && start goapi.exe
+for /f "tokens=*" %%i in ('git describe --tags --always') do set GIT_VERSION=%%i
+go build -ldflags "-H windowsgui -X goapi/api.Version=%GIT_VERSION%" -o goapi.exe . && start goapi.exe
